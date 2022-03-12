58d316cc06d5756522a77fbd01ee112b080738b5
Oleh Zharovskyi <oleh.zharovskyi@newfireglobal.com>
2022-03-12 14:17:04 +0200

    added header

--- src/components/TablePage/Table.js
+++ src/components/TablePage/Table.js
@@ -1,12 +1,12 @@
 import { DataGrid } from "@mui/x-data-grid";
 import { rows, columns } from "./data";
 
-function Table() {
+const Table = () => {
   return (
     <>
       <DataGrid rows={rows} columns={columns} />
     </>
   );
-}
+};
 
 export default Table;
--- src/components/TablePage/index.js
+++ src/components/TablePage/index.js
@@ -1,18 +1,27 @@
+import Header from "../../shared/Header/Header";
 import Table from "./Table";
 
 function TablePage() {
   return (
-    <div
-      className="App"
-      style={{
-        height: 300,
-        // minWidth: 320,
-        maxWidth: 720,
-        margin: "0 auto",
-      }}
-    >
-      <Table />
-    </div>
+    <>
+      <Header />
+      <div
+        className="App"
+        style={{
+          height: 300,
+          // minWidth: 320,
+          maxWidth: "70%",
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          margin: "auto",
+        }}
+      >
+        <Table />
+      </div>
+    </>
   );
 }
 
